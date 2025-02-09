b87ece47bc6ccf964255a9b57b1fac738f8b4c67
TejasGhatte <tejasghatte5@gmail.com>
2025-02-09 08:03:30 +0530

    fix:analysis

--- utils/ml-actions/proccessImage.ts
+++ utils/ml-actions/proccessImage.ts
@@ -3,7 +3,6 @@ import { projectClient } from '@/lib/bucket-client';
 
 export async function processImage(imageUrl: string, imageId: string, userId: string, imageName: string) {
     try {
-
         const response = await fetch(`${process.env.ML_SERVER_URL}/analyze-waste`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -13,7 +12,6 @@ export async function processImage(imageUrl: string, imageId: string, userId: st
         const data = await response.json();
         console.log(data);
         
-
         const base64Image = data.visualization.replace(/^data:image\/\w+;base64,/, '');
         const buffer = Buffer.from(base64Image, 'base64');
 
@@ -23,46 +21,52 @@ export async function processImage(imageUrl: string, imageId: string, userId: st
             processedImageFileName
         );
 
-        // const analysis = await prisma.wasteAnalysis.create({
-        //     data: {
-        //         imageId,
-        //         totalWeightKg: data.total_stats.total_weight_kg,
-        //         wasteCoverage: data.total_stats.waste_coverage,
-        //         distributionEvenness: data.total_stats.distribution_evenness,
-        //         compositions: {
-        //             create: [
-        //                 {
-        //                     type: 'RIGID_PLASTIC',
-        //                     present: data.waste_composition.rigid_plastic.present,
-        //                     pixelCount: data.waste_composition.rigid_plastic.pixel_count,
-        //                     areaPercentage: data.waste_composition.rigid_plastic.area_percentage,
-        //                     estimatedWeightKg: data.waste_composition.rigid_plastic.estimated_weight_kg,
-        //                 },
-        //                 {
-        //                     type: 'CARDBOARD',
-        //                     present: data.waste_composition.cardboard.present,
-        //                     pixelCount: data.waste_composition.cardboard.pixel_count,
-        //                     areaPercentage: data.waste_composition.cardboard.area_percentage,
-        //                     estimatedWeightKg: data.waste_composition.cardboard.estimated_weight_kg,
-        //                 },
-        //                 {
-        //                     type: 'METAL',
-        //                     present: data.waste_composition.metal.present,
-        //                     pixelCount: data.waste_composition.metal.pixel_count,
-        //                     areaPercentage: data.waste_composition.metal.area_percentage,
-        //                     estimatedWeightKg: data.waste_composition.metal.estimated_weight_kg,
-        //                 },
-        //                 {
-        //                     type: 'SOFT_PLASTIC',
-        //                     present: data.waste_composition.soft_plastic.present,
-        //                     pixelCount: data.waste_composition.soft_plastic.pixel_count,
-        //                     areaPercentage: data.waste_composition.soft_plastic.area_percentage,
-        //                     estimatedWeightKg: data.waste_composition.soft_plastic.estimated_weight_kg,
-        //                 },
-        //             ],
-        //         },
-        //     },
-        // });
+
+        const analysis = await prisma.wasteAnalysisOfImage.create({
+            data: {
+                imageId,
+                totalWeightKg: data.analytics.total_stats.total_weight_kg,
+                wasteCoverage: data.analytics.total_stats.waste_coverage,
+                distributionEvenness: data.analytics.total_stats.distribution_evenness,
+            },
+        });
+
+        await prisma.wasteCompositionOfImage.createMany({
+            data: [
+                {
+                    wasteAnalysisOfImageId: analysis.id,
+                    type: 'RIGID_PLASTIC',
+                    present: data.analytics.waste_composition.rigid_plastic.present,
+                    pixelCount: data.analytics.waste_composition.rigid_plastic.pixel_count,
+                    areaPercentage: data.analytics.waste_composition.rigid_plastic.area_percentage,
+                    estimatedWeightKg: data.analytics.waste_composition.rigid_plastic.estimated_weight_kg,
+                },
+                {
+                    wasteAnalysisOfImageId: analysis.id,
+                    type: 'CARDBOARD',
+                    present: data.analytics.waste_composition.cardboard.present,
+                    pixelCount: data.analytics.waste_composition.cardboard.pixel_count,
+                    areaPercentage: data.analytics.waste_composition.cardboard.area_percentage,
+                    estimatedWeightKg: data.analytics.waste_composition.cardboard.estimated_weight_kg,
+                },
+                {
+                    wasteAnalysisOfImageId: analysis.id,
+                    type: 'METAL',
+                    present: data.analytics.waste_composition.metal.present,
+                    pixelCount: data.analytics.waste_composition.metal.pixel_count,
+                    areaPercentage: data.analytics.waste_composition.metal.area_percentage,
+                    estimatedWeightKg: data.analytics.waste_composition.metal.estimated_weight_kg,
+                },
+                {
+                    wasteAnalysisOfImageId: analysis.id,
+                    type: 'SOFT_PLASTIC',
+                    present: data.analytics.waste_composition.soft_plastic.present,
+                    pixelCount: data.analytics.waste_composition.soft_plastic.pixel_count,
+                    areaPercentage: data.analytics.waste_composition.soft_plastic.area_percentage,
+                    estimatedWeightKg: data.analytics.waste_composition.soft_plastic.estimated_weight_kg,
+                },
+            ],
+        });
 
         await prisma.image.update({
             where: { id: imageId },
@@ -71,6 +75,7 @@ export async function processImage(imageUrl: string, imageId: string, userId: st
 
         return {
             processedImageUrl,
+            analysis
         };
     } catch (error) {
         console.error("Error analyzing waste:", error);
